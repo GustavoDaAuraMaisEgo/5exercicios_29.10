67150cda36b0ade73671c9494a3a4d2e9804e7b5
Rodrigo Borges <digoborges999@gmail.com>
2025-10-23 19:04:37 -0300

    feito

--- Ex1 Rodrigo.py
+++ Ex1 Rodrigo.py
@@ -5,32 +5,37 @@ print('='*35)
 print("Bem vindo. Faça o Cadastro dos Alunos")
 print('='*35)
 
-cadastro=dict()
+cadastro = dict()
 
-for c in range(1,5):
+for c in range(1, 5):
     print(f"----- {c}° Aluno -----")
-    nome=str(input("Nome: "))
-    nota1=float(input("Nota 1: "))
-    nota2=float(input("Nota 2: "))
-    nota3=float(input("Nota 3: "))
-    nota4=float(input("Nota 4: "))
-    media=(nota1 + nota2 + nota3 + nota4)/4
+    nome = str(input("Nome: "))
+    nota1 = float(input("Nota 1: "))
+    nota2 = float(input("Nota 2: "))
+    nota3 = float(input("Nota 3: "))
+    nota4 = float(input("Nota 4: "))
+    os.system('cls')
+    media = (nota1 + nota2 + nota3 + nota4) / 4
     cadastro[nome] = media
     print("-------------------")
+
 os.system('cls')
 print("----- Boletim dos Alunos -----")
-for k,v in cadastro.items():
+for k, v in cadastro.items():
     print(f"O aluno {k} teve média {v:.1f}")
 print("------------------------------")
 
 while True:
-    consulta=str(input("Deseja consultar a média de algum aluno? (s/n): "))
+    consulta = str(input("Deseja consultar a média de algum aluno? (s/n): ")).lower().strip()
     if consulta == 's':
-        aluno=str(input("Digite o nome do aluno: "))
-        if aluno in cadastro and media >= 7.0:
-            print(f"A média do aluno {aluno} é {cadastro[aluno]:.1f} - Aprovado")
-        elif aluno in cadastro and media < 7.0:
-            print(f"A média do aluno {aluno} é {cadastro[aluno]:.1f} - Reprovado")
+        os.system('cls')
+        aluno = str(input("Digite o nome do aluno: "))
+        if aluno in cadastro:
+            
+            if cadastro[aluno] >= 7.0:
+                print(f"A média do aluno {aluno} é {cadastro[aluno]:.1f} - Aprovado")
+            else:
+                print(f"A média do aluno {aluno} é {cadastro[aluno]:.1f} - Reprovado")
         else:
             print("Aluno não encontrado.")
     elif consulta == 'n':
@@ -38,5 +43,5 @@ while True:
         print("Encerrando o programa. Obrigado!")
         break
     else:
-        os.system('cls')
+        
         print("Opção inválida. Digite 's' para sim ou 'n' para não.")
