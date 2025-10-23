0731d30084503c0cca3ccd121cf9e7ebf2b1f8da
Rodrigo Borges <digoborges999@gmail.com>
2025-10-22 21:23:53 -0300

    Ex.1 feito

--- Ex1 Rodrigo.py
+++ Ex1 Rodrigo.py
@@ -14,13 +14,16 @@ media=(cadastro['N1']+cadastro['N2']+cadastro['N3'])/3
 
 print('='*60)
 if media>=7:
+    os.system('cls')
     print(f"Aluno {cadastro['Nome']} aprovado com média {media:.2f}")
 else:
+    os.system('cls')
     print(f"Aluno {cadastro['Nome']} reprovado com média {media:.2f}")
-print('='*60)
+print('')
 print("Gostaria de ver suas notas desse Bimestre?")
 resposta=str(input("Digite [S] para SIM ou [N] para NÃO: ")).upper().strip()
 if resposta=='S':
+    os.system('cls')
     print(f"As notas do Aluno {cadastro['Nome']} são: ")
     print(f"Nota 1: {cadastro['N1']}")
     print(f"Nota 2: {cadastro['N2']}")
