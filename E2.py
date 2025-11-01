effe6c7bba30548fa77ffddcaa3e5d17920aecb6
Luiz-Carlos-Borges <luizcbdamico1@gmail.com>
2025-11-01 16:22:15 -0300

    .

--- E2.py
+++ E2.py
@@ -2,28 +2,24 @@ import os
 import random
 import time
 
-os.system('cls' if os.name == 'nt' else 'clear')
+os.system('cls')
 jogadores = []
 print('Campeonato mundial de rolar um dado!')
 
-for c in range (0,4):
-    nome = str(input(f'Nome do jogador {c+1}: '))
+for c in range(4):
+    nome = input(f'Nome do jogador {c+1}: ').strip() or f'Jogador{c+1}'
     jogadores.append(nome)
-print('Preparando os dados...')
-time.sleep(2)
 
+print('Preparando os dados...')
+time.sleep(1)
+
+# rolar dados
 resultados = {}
 for jogador in jogadores:
-    dado = random.randint(1,6)
+    dado = random.randint(1, 6)
     resultados[jogador] = dado
     print(f'O jogador {jogador} rolou o dado e tirou {dado}.')
-    time.sleep(1)
-print('Calculando os resultados finais...')
-time.sleep(2)
+    time.sleep(0.6)
 
-print('------Resultados finais------')
-for jogador, resultado in resultados.items():
-    print(f'\nO {jogador} ficou em primeiro lugar' if resultado == max(resultados.values()) else f'O {jogador} ficou com {resultado} pontos.')
-    print(f'\nO {jogador} ficou em segundo lugar' if resultado == sorted(resultados.values(), reverse=True)[1] else f'O {jogador} ficou com {resultado} pontos.')
-    print(f'\nO {jogador} ficou em terceiro lugar' if resultado == sorted(resultados.values(), reverse=True)[2] else f'O {jogador} ficou com {resultado} pontos.')
-    print(f'\nO {jogador} ficou em quarto lugar' if resultado == min(resultados.values()) else f'O {jogador} ficou com {resultado} pontos.')
+print('\nCalculando os resultados finais...')
+time.sleep(1)
